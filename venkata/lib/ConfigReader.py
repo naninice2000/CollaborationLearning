eb9c837a86c4512a7eda7d5941d7bf76a899275c
Venkata <naninice2000@gmail.com>
2017-06-17 21:31:54 -0700

    Adding config.json file which store test environment details. Test cofiguration parser python module

---
+++ venkata/lib/ConfigReader.py
@@ -0,0 +1,13 @@
+import json
+
+class ConfigReader:
+	config_file = ""
+	config_map = {}
+
+	def __init__(self):
+		self.config_file = conf_file
+		with open(self.config_file) as data_file:    
+			self.config_map = json.load(data_file)
+	
+	def get_test_environment_details(self):
+		return self.config_map
